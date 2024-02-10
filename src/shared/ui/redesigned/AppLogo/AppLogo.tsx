2a341cbc89145e85cad6fe4790871c2f72572cca
vit-vokhminov <vit-vokhminov@yandex.ru>
2024-02-10 18:32:56 +0700

    139 градиент на иконке

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react';
 import cls from './AppLogo.module.scss';
-import { HStack } from '../../deprecated/Stack';
+import { HStack } from '../Stack';
 import AppSvg from '@/shared/assets/icons/app-image.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 
@@ -16,14 +16,14 @@ export const AppLogo = memo(({ className, size = 50 }: AppLogoProps) => {
             justify='center'
             className={classNames(cls.appLogoWrapper, {}, [className])}
         >
-            <div className={cls.gradientBig} />
-            <div className={cls.gradientSmall} />
             <AppSvg
                 width={size}
                 height={size}
                 color='black'
                 className={cls.appLogo}
             />
+            <div className={cls.gradientBig} />
+            <div className={cls.gradientSmall} />
         </HStack>
     );
 });
